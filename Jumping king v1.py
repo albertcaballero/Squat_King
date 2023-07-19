7cbeee5fd09ec4133481729e5c02533ae1c41865
albertcaballero <albertcaballerocoll@gmail.com>
2023-07-19 20:16:06 +0200

    added mediapipe library fullbody estimation

--- Jumping king v1.py
+++ Jumping king v1.py
@@ -1,47 +1,49 @@
 import cv2 as cv
 import numpy as np
+import mediapipe as mp
 
 body_cascade = cv.CascadeClassifier(r'C:\Users\Albert\Documents\opencv\sources\data\haarcascades\haarcascade_fullbody.xml')
 face_cascade = cv.CascadeClassifier(r'C:\Users\Albert\Documents\opencv\sources\data\haarcascades\haarcascade_frontalface_alt.xml')
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
 
-"""
-def findjoint_Tracking(frame):
-    artic = body_cascade.detectMultiScale(frame)
-    for (x, y, width, height) in artic:
-        trackframe = cv.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
-    return trackframe
-"""
 
 def getwinsize(capture):
     width = capture.get(cv.CAP_PROP_FRAME_WIDTH)
     height = capture.get(cv.CAP_PROP_FRAME_HEIGHT)
     fps = capture.get(cv.CAP_PROP_FPS)
- #   print("W =" + (str)width + "\nH = " + (str)height + "\nfps = " + (str)fps)
     print(f"W = {width} \nH = {height} \nfps = {fps}")
     return width, height
+
+def squatinput(frame, results):
+    random = 1
+
+def armsimput(frame, results):
+    random = 1
 
 capture = cv.VideoCapture(0)
 if not capture.isOpened():
     print("Cannot open camera")
     exit()
 width, height = getwinsize(capture)
-while True:
-    ret, frame = capture.read() # Capture frame-by-frame, 'ret' is boolean, 'frame' is the actual frame/image
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    grayframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#   grayframe = cv.rectangle(grayframe, (0,0), (200,200), (23, 255, 200), thickness=-1) #idk why but grayframe is transforming the square to black
-#   grayframe = findjoint_Tracking(grayframe)
-    artic = body_cascade.detectMultiScale(grayframe)
-    for (x, y, width, height) in artic:
-        cv.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        grayframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        results = holistic.process(frame)
+        face = face_cascade.detectMultiScale(grayframe)
+        for (x, y, width, height) in face:
+            cv.rectangle(frame, (x, y), (x + width, y + height), (0,255,0), 2)
+            cv.line(frame, (x + width//2,y + height), (x + width//2,480), (0,255,0), thickness=1)
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
 
-capture.release() # When everything done, release the capture
+capture.release()
 cv.destroyAllWindows()
 
 
