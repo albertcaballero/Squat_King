77d62fee1b76c14e7dfe337e3d59d7927651a6b7
albertcaballero <albertcaballerocoll@gmail.com>
2023-07-19 14:04:15 +0200

    it friggggging woooorks

--- Jumping king v1.py
+++ Jumping king v1.py
@@ -2,17 +2,20 @@ import cv2 as cv
 import numpy as np
 
 body_cascade = cv.CascadeClassifier(r'C:\Users\Albert\Documents\opencv\sources\data\haarcascades\haarcascade_fullbody.xml')
+face_cascade = cv.CascadeClassifier(r'C:\Users\Albert\Documents\opencv\sources\data\haarcascades\haarcascade_frontalface_alt.xml')
 
 class joint:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
-def findjoint_Tracking(gray):
-    bodpart = body_cascade.detectMultiScale(gray)
-
-def drawjoint(joint, frame):
-    cv.circle(frame, (joint.x, joint.y), 10, (0, 0, 255), 2)
+"""
+def findjoint_Tracking(frame):
+    artic = body_cascade.detectMultiScale(frame)
+    for (x, y, width, height) in artic:
+        trackframe = cv.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
+    return trackframe
+"""
 
 def getwinsize(capture):
     width = capture.get(cv.CAP_PROP_FRAME_WIDTH)
@@ -33,11 +36,21 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    grayframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # frame transforms into grayframe (b&w)
-    cv.rectangle(frame, (0,0), (200,200), (23, 255, 200), thickness=-1)
-    cv.imshow('frame', grayframe) # Display the resulting frame
+    grayframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+#   grayframe = cv.rectangle(grayframe, (0,0), (200,200), (23, 255, 200), thickness=-1) #idk why but grayframe is transforming the square to black
+#   grayframe = findjoint_Tracking(grayframe)
+    artic = body_cascade.detectMultiScale(grayframe)
+    for (x, y, width, height) in artic:
+        cv.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
+    cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
 
 capture.release() # When everything done, release the capture
 cv.destroyAllWindows()
+
+
+"""
+- explore other kinds of cascades (fullbody is a bit meh)
+- see if we can increase the threshold for detection and limit the number of detections (so it only detects 1 body and not random shit)
+"""
